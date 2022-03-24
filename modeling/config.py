85bd15223ce69878550afd9df3e3154d3a184fc5
Filip Rankovic <30380150+filip-rankovic@users.noreply.github.com>
2022-03-24 12:08:01 +0100

    dwd and ml flow (#75)
    * weather forecasting notebook
    
    * midterm presentation and meteometics update, py file
    
    * weather stations info added
    
    * ml flow notebook, config and dwd download update

--- modeling/config.py
+++ modeling/config.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 # mlflow
 # sometimes when saving links in text.. there is a new line .. strip removes that
 try:
-    TRACKING_URI = open(".mlflow_uri").read().strip()
+    TRACKING_URI = open("../.mlflow_uri").read().strip()
 except:
     TRACKING_URI = parsenvy.str("MLFLOW_URI")
 
-EXPERIMENT_NAME = "0-template-ds-modeling"
+EXPERIMENT_NAME = "air-pollution"
